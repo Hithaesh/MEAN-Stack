353cc82f068abfb7d4db7a184d49df8587abab2b
Hithaesh <hithaeshpraveen2002@gmail.com>
2025-02-09 12:01:22 +0530

    added sign up screen

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,9 +3,8 @@ import { NgForm } from "@angular/forms";
 
 //Todo: Loading this via routing, so ignoring the step of adding a selector ('app-login-component')
 @Component({
-  // selector: 'app-login-component'
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrl: './login.component.css'
 })
 export class LoginComponent {
   isLoading: boolean = false;
